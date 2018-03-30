87f6268bb6178f12e3dd9c428182c612a043d18a
Minh Tuan Nguyen <ntuan221@gmail.com>
2018-03-30 16:34:22 +0700

    reduce length of json message send to network

--- tests/example02/app02.py
+++ tests/example02/app02.py
@@ -1,6 +1,6 @@
 from cython_npm.cythoncompile import require
 from functools import wraps
-from microservices_connector.Interservices import SanicApp as Microservice
+from Interservices import SanicApp as Microservice
 
 # module = require('../../microservices_connector/Interservices')
 # Microservice = module.SanicApp
